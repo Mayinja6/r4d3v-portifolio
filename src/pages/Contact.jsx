60596061e05006271d068c08137e0503afc48f79
Douglas <89643003+Mayinja6@users.noreply.github.com>
2022-09-05 14:32:49 +0300

    Email validation notification

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -36,11 +36,10 @@ const Contact = () => {
         let filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
 
         if (!filter.test(email)) {
-          addToast("Please Provide a valid Email Address.", {
-            appearance: "error",
-            autoDismiss: true,
-          });
-            return;
+            return addToast("Please Provide a valid Email Address.", {
+              appearance: "error",
+              autoDismiss: true,
+            });
         }
     }
       checkEmail()
